6d50dd05caa68a32c6a084a0db95d2f8ba221e85
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-02 19:47:25 -0500

    Created parent command
    Now commands can be refactored so they all don’t need to get the view
    text, do something, then updated the view text. Now a child command
    simply implements modify(text)

--- pretty_markdown.py
+++ pretty_markdown.py
@@ -1,4 +1,4 @@
-import sublime
+import sublime, sublime_plugin
 
 def file_extension(path):
     '''Returns the file extension of the file at a given path. If the path isn't for a file, or it doesn't have one, None is returned.'''
@@ -29,3 +29,25 @@ def settings():
     '''Retrieves the settings file.'''
 
     return sublime.load_settings("Pretty Markdown.sublime-settings")
+
+class PrettyMarkdownCommand(sublime_plugin.TextCommand):
+    '''Parent for all PrettyMarkdown commands.
+
+    Due to my naming convention overlapping with Sublime Text command naming conventions, is_enabled() was defined to return True only
+    for subclasses of PrettyMarkdownCommand.'''
+
+    def modify(self, text):
+        return text
+
+    def run(self, edit):
+        '''Converts any bold implementation into the one defined in the settings.'''
+
+        r = sublime.Region(0, self.view.size())
+        text = self.view.substr(r)
+
+        text = self.modify(text)
+
+        self.view.replace(edit, r, text)
+
+    def is_enabled(self):
+        return self.__class__.__name__ != 'PrettyMarkdownCommand'
