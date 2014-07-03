514382feb941535f74b56e01b608b25d128ab92e
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-02 19:50:44 -0500

    Now is a subclass of PrettyMarkdownCommand

--- Bolds.py
+++ Bolds.py
@@ -1,15 +1,12 @@
-import sublime, sublime_plugin, re
 from . import pretty_markdown
 
-class ConvertBoldCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+import re
+
+class ConvertBoldCommand(pretty_markdown.PrettyMarkdownCommand):
+    def modify(self, text):
         '''Converts any bold implementation into the one defined in the settings.'''
 
-        r = sublime.Region(0, self.view.size())
-        text = self.view.substr(r)
-
         bold_character = pretty_markdown.settings().get('bold_character')
-
         text = re.sub(r'(?<!\\)(\*{2}|_{2})([\S \t]+?)\1', r'{0}{0}\2{0}{0}'.format(bold_character), text)
 
-        self.view.replace(edit, r, text)
+        return text
