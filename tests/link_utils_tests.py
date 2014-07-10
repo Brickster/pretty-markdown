b5797c7fed9fb54f7b8c4f6c26df500203be02c1
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-09 20:52:22 -0500

    Added link utils
    Added a single method for determining if text is a reference link
    definition

---
+++ tests/link_utils_tests.py
@@ -0,0 +1,61 @@
+from utils import link_utils
+
+import unittest
+
+class LinkUtilsTests(unittest.TestCase):
+
+    def test_isLinkReferenceDefinition_empty(self):
+
+        text = ''
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertFalse(is_reference)
+
+    def test_isLinkReferenceDefinition_blank(self):
+
+        text = '      '
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertFalse(is_reference)
+
+    def test_isLinkReferenceDefinition_false(self):
+
+        text = 'this is not a reference'
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertFalse(is_reference)
+
+    def test_isLinkReferenceDefinition_true(self):
+
+        text = '[Google]: http://google.com'
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertTrue(is_reference)
+
+    def test_isLinkReferenceDefinition_containsTitle(self):
+
+        text = '[Google]: http://google.com "Google.com"'
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertTrue(is_reference)
+
+    def test_isLinkReferenceDefinition_multipleLines(self):
+
+        text = '[Google]: http://google.com\nThis entire string is not a reference."'
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertFalse(is_reference)
+
+    def test_isLinkReferenceDefinition_idWithoutDefinition(self):
+
+        text = '[Google]: '
+        is_reference = link_utils.is_link_reference_definition(text)
+
+        self.assertFalse(is_reference)
+
+    # def test_isLinkReferenceDefinition_containsTitle_inWrongOrder(self):
+
+    #     text = '[Google]: "This is in the wrong order" http://google.com'
+    #     is_reference = link_utils.is_link_reference_definition(text)
+
+    #     self.assertFalse(is_reference)
---
+++ utils/link_utils.py
@@ -0,0 +1,8 @@
+import re
+
+link_reference_definition_pattern = re.compile(r'^\[.*\]:\s+.+?(\s+".*")?$')
+
+def is_link_reference_definition(text):
+    '''Determines whether a given string is a link reference definition.'''
+
+    return link_reference_definition_pattern.match(text) is not None
