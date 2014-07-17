899306516f7fa2faf11ff26486fb55730b014996
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-16 20:29:45 -0500

    Added link definition formatting

--- tests/link_utils_tests.py
+++ tests/link_utils_tests.py
@@ -62,9 +62,69 @@ class LinkUtilsTests(unittest.TestCase):
 
     def test_formatLinkReferenceDefitions(self):
 
-        text = 'This is the text'
+        text = 'I use [Google][] and sometimes [Bing][].\n\n[Google]: http://google.com\n[IGN]: http://ign.com\n\nGoogle is better though.'
+
+        expected = 'I use [Google][] and sometimes [Bing][].\n\n[Google]: http://google.com\n[IGN]:    http://ign.com\n\nGoogle is better though.'
+        actual = link_utils.format_link_reference_definitions(text)
+
+        self.assertEqual(actual, expected)
+
+    def test_formatLinkReferenceDefitions_noLinks(self):
+
+        text = 'This is just text.'
 
         expected = text
+        actual = link_utils.format_link_reference_definitions(text)
+
+        self.assertEqual(actual, expected)
+
+    def test_formatLinkReferenceDefitions_oneLink(self):
+
+        text = '[Google]: http://google.com'
+
+        expected = text
+        actual = link_utils.format_link_reference_definitions(text)
+
+        self.assertEqual(actual, expected)
+
+    def test_formatLinkReferenceDefitions_twoLinks(self):
+
+        text = '[Google]: http://google.com\n[IGN]: http://ign.com'
+
+        expected = '[Google]: http://google.com\n[IGN]:    http://ign.com'
+        actual = link_utils.format_link_reference_definitions(text)
+
+        self.assertEqual(actual, expected)
+
+    def test_formatLinkReferenceDefitions_multipleGroups(self):
+
+        text = """This is [Markdown][].
+
+[Markdown]: http://daringfireball.net/projects/markdown/
+
+You can use it to write HTML that is not ugly due to messy [ordered lists][] and [links][].
+
+[ordered lists]: http://daringfireball.net/projects/markdown/syntax#list
+[links]: http://daringfireball.net/projects/markdown/syntax#link
+
+For instance, I can link to [Google][] and [Bing][].
+
+[Google]: http://google.com
+[Bing]: http://bing.com"""
+
+        expected = """This is [Markdown][].
+
+[Markdown]: http://daringfireball.net/projects/markdown/
+
+You can use it to write HTML that is not ugly due to messy [ordered lists][] and [links][].
+
+[ordered lists]: http://daringfireball.net/projects/markdown/syntax#list
+[links]:         http://daringfireball.net/projects/markdown/syntax#link
+
+For instance, I can link to [Google][] and [Bing][].
+
+[Google]: http://google.com
+[Bing]:   http://bing.com"""
         actual = link_utils.format_link_reference_definitions(text)
 
         self.assertEqual(actual, expected)
--- utils/link_utils.py
+++ utils/link_utils.py
@@ -1,13 +1,49 @@
 import re
 
-LINK_REFERENCE_DEFINITION_PATTERN = re.compile(r'^\[.*\]:\s+.+?(\s+".*")?$')
+LINK_REFERENCE_DEFINITION_PATTERN = re.compile(r'^\[(.*)\]:\s+(.+?(?:\s+".*")?$)')
 
 def is_link_reference_definition(text):
     """Determines whether a given string is a link reference definition."""
 
     return LINK_REFERENCE_DEFINITION_PATTERN.match(text) is not None
 
+def _format_links(links):
+    """Formats a list of links."""
+
+    pairs = [LINK_REFERENCE_DEFINITION_PATTERN.match(cur).groups() for cur in links]
+    longest = len(max([pair[0] for pair in pairs], key=len)) + 4 # 4 accounts for [, ], :, and ' '
+    formatted_links = ['[{}]: '.format(definition).ljust(longest) + link for definition, link in pairs]
+
+    return formatted_links
+
 def format_link_reference_definitions(text):
     """Formats groups of link reference definitions so that their links all line up."""
 
-    return text # do nothing for now
+    text = text.split('\n')
+    link_group = []
+    output = []
+
+    for line in text:
+
+        is_link_definition = is_link_reference_definition(line)
+        if not is_link_definition and link_group:
+
+            formatted_links = _format_links(link_group)
+
+            output += formatted_links
+            output.append(line)
+            link_group = []
+
+        elif is_link_definition and text.index(line) == len(text) - 1:
+
+            link_group.append(line)
+            output += _format_links(link_group)
+
+        elif is_link_definition:
+            link_group.append(line)
+        else:
+            output.append(line)
+
+    text = '\n'.join(output)
+
+    return text
