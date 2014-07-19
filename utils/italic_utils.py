c851ed489ee2c44932c7faf1132fe175c9efa4ed
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-18 19:38:19 -0500

    Fixed issue #14
    The bold side was already working but this fixes the italics side.

--- utils/italic_utils.py
+++ utils/italic_utils.py
@@ -10,17 +10,7 @@ def convert_italics(text, character='_'):
 
     assert character in VALID_CHARACTERS, 'character must be one of {}'.format(VALID_CHARACTERS)
 
-    if character == '*':
-        other_char = '_'
-    else:
-        other_char = '\*' # we don't want the asterisk interpreted as a quantifier
-
-    p = re.compile(r'(?<![*_]{1})([*_]{2})?(' + other_char + r')([^\s*_](?:[\S \t]+?\S)?)(?<!\\)\2', flags=re.MULTILINE)
-    while p.search(text) is not None:
-        m = p.search(text)
-        if m.group(1) is None:
-            text = p.sub(r'{0}\3{0}'.format(character), text, count=1)
-        else:
-            text = p.sub(r'\1{0}\3{0}'.format(character), text, count=1)
+    p = re.compile(r'(?<![*_])((?:\*{2}|_{2})?)([*_])([*_]*)((?<!(?<![*_])[*_][*_])[^\s*_](?:[\S \t]+?\S)?)\3\2\1')
+    text = p.sub(r'\1{0}\3\4\3{0}\1'.format(character), text)
 
     return text
