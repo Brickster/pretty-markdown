8e1cccf3fd1c099daa685d0408cd0a5a67909c99
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-19 11:45:32 -0500

    Added _is_setext_header(text) method

--- tests/header_utils_tests.py
+++ tests/header_utils_tests.py
@@ -4,6 +4,10 @@ import unittest
 
 class HeaderUtilsTests(unittest.TestCase):
 
+    #
+    # fix_header_balancing
+    #
+
     def test_fixHeaderBalancing_empty(self):
 
         text = ''
@@ -66,3 +70,63 @@ class HeaderUtilsTests(unittest.TestCase):
         actual = header_utils.fix_header_balancing(text)
 
         self.assertEqual(actual, expected)
+
+    #
+    # _is_setext_header
+    #
+
+    def test__isSetextHeader_equals(self):
+
+        text = '==='
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertTrue(is_setext)
+
+    def test__isSetextHeader_dash(self):
+
+        text = '---'
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertTrue(is_setext)
+
+    def test__isSetextHeader_tooFew(self):
+
+        text = '=='
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
+
+    def test__isSetextHeader_wrongCharacter(self):
+
+        text = '**'
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
+
+    def test__isSetextHeader_textLineStartsWithSetextHeader(self):
+
+        text = '==== this starts with a setext style header'
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
+
+    def test__isSetextHeader_empty(self):
+
+        text = ''
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
+
+    def test__isSetextHeader_blank(self):
+
+        text = '   '
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
+
+    def test__isSetextHeader_none(self):
+
+        text = None
+        is_setext = header_utils._is_setext_header(text)
+
+        self.assertFalse(is_setext)
--- utils/header_utils.py
+++ utils/header_utils.py
@@ -1,5 +1,13 @@
 import re
 
+SETEXT_HEADER_PATTERN = re.compile(r'^(?:={3,}|-{3,})$')
+
+def _is_setext_header(text):
+    """Determines if text is a setext type header line."""
+
+    return text is not None and SETEXT_HEADER_PATTERN.match(text) is not None
+
+
 def fix_header_balancing(text):
     """Balances headers."""
 
