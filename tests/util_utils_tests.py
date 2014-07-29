8c842d68b35dfefb6d9e66a8e780e69b08b604dd
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-28 22:26:16 -0500

    Added tests for util_utils
    This fixes issue #22.

---
+++ tests/util_utils_tests.py
@@ -0,0 +1,71 @@
+from utils import util_utils
+
+import unittest
+
+class UtilUtilsTests(unittest.TestCase):
+
+    #
+    # process_groups
+    #
+
+    # def process_groups(text, is_group_member, process_group, is_group_member_parameters={}, process_group_parameters={}):
+
+    def test_processGroups(self):
+
+        text = 'This\ncould be\nthe test'
+
+        is_group_lambda = lambda line: line == 'could be'
+        process_lambda = lambda lines: ['is'] 
+
+        expected = 'This\nis\nthe test'
+        actual = util_utils.process_groups(text, is_group_member=is_group_lambda, process_group=process_lambda)
+
+        self.assertEqual(actual, expected)
+
+    def test_processGroups_noChange(self):
+
+        text = 'This\nis\nthe test'
+
+        is_group_lambda = lambda line: False
+        process_lambda = None
+
+        expected = text
+        actual = util_utils.process_groups(text, is_group_member=is_group_lambda, process_group=process_lambda)
+
+        self.assertEqual(actual, expected)
+
+    def test_processGroups_multipleGroups(self):
+
+        text = 'This is text.\n\nRemove\nRemove\nWith text that will be removed.\nRemove\nIn multple places.'
+
+        is_group_lambda = lambda line: line == 'Remove'
+        process_lambda = lambda lines: ['Changed' for line in lines]
+
+        expected = 'This is text.\n\nChanged\nChanged\nWith text that will be removed.\nChanged\nIn multple places.'
+        actual = util_utils.process_groups(text, is_group_member=is_group_lambda, process_group=process_lambda)
+
+        self.assertEqual(actual, expected)
+
+    def test_processGroups_startsWithGroup(self):
+
+        text = 'Remove\nRemove\nWith text that will be removed.'
+
+        is_group_lambda = lambda line: line == 'Remove'
+        process_lambda = lambda lines: ['Changed' for line in lines]
+
+        expected = 'Changed\nChanged\nWith text that will be removed.'
+        actual = util_utils.process_groups(text, is_group_member=is_group_lambda, process_group=process_lambda)
+
+        self.assertEqual(actual, expected)
+
+    def test_processGroups_endsWithGroup(self):
+
+        text = 'This is text.\n\nRemove\nRemove'
+
+        is_group_lambda = lambda line: line == 'Remove'
+        process_lambda = lambda lines: ['Changed' for line in lines]
+
+        expected = 'This is text.\n\nChanged\nChanged'
+        actual = util_utils.process_groups(text, is_group_member=is_group_lambda, process_group=process_lambda)
+
+        self.assertEqual(actual, expected)
