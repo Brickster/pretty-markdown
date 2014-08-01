0036159e03bf852227010cae58f387450b331085
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2014-07-31 22:18:58 -0500

    Added crude backup creation before formatting
    This is a first pass at #17.

--- pretty_markdown_save_listener.py
+++ pretty_markdown_save_listener.py
@@ -1,11 +1,30 @@
 import os.path
 import pretty_markdown
+import sublime
 import sublime_plugin
 
 
 class PrettyMarkdownSaveListenerCommand(sublime_plugin.EventListener):
+
+    def save_text(self, text):
+        """Creates a backup of the text."""
+
+        backup_directory = sublime.packages_path() + '/PrettyMarkdown/backup'
+        backup_path = backup_directory + '/save.backup'
+
+        if not os.path.exists(backup_directory):
+            os.makedirs(backup_directory)
+
+        with open(backup_path, 'w') as backup_file:
+            backup_file.write(text)
+
     def on_pre_save(self, view):
         """Runs Pretty Markdown pre-save actions"""
+
+        # save a copy of the file before editting
+        r = sublime.Region(0, view.size())
+        text = view.substr(r)
+        self.save_text(text)
 
         extension = os.path.splitext(view.file_name())[1][1:]
         if extension in pretty_markdown.settings().get('format_files_with_extension'):
